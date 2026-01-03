fa309b550f29291ddc8b3b4524448b117d4046be
Carlo Costa <carlo.costa92@gmail.com>
2026-01-03 17:19:44 +0100

    responsive fix

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -10,21 +10,23 @@ import {
 const Footer = () => {
   return (
     <footer className="bg-(--color-primary) text-white px-6 py-10">
-      <div className=" container mx-auto flex justify-between items-center">
-        <div>
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center gap-8 md:gap-0">
+        {/* Social */}
+        <div className="flex flex-col">
           <h4 className="font-semibold mb-4">Social</h4>
-          <div className="flex gap-4 opacity-80">
+          <div className="flex gap-4 opacity-80 mb-2">
             <Instagram size={22} />
             <Facebook size={22} />
             <Youtube size={22} />
             <Twitter size={22} />
           </div>
-          <p className="text-xs mt-2 opacity-60">
+          <p className="text-xs opacity-60">
             Profili social in fase di attivazione
           </p>
         </div>
 
-        <div>
+        {/* Sede legale */}
+        <div className="flex flex-col">
           <h4 className="font-semibold mb-4">Sede legale</h4>
           <p className="flex items-start gap-2 text-sm opacity-80">
             <MapPin size={18} className="mt-0.5" />
@@ -32,7 +34,8 @@ const Footer = () => {
           </p>
         </div>
 
-        <div>
+        {/* Dati legali */}
+        <div className="flex flex-col">
           <h4 className="font-semibold mb-4">Dati legali</h4>
           <p className="flex items-center gap-2 text-sm opacity-80">
             <FileText size={18} />
